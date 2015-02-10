cb1f7474cf7c61a0983f9679aee654903160894b
anishFEMInc <anish@fem-inc.com>
2015-02-09 17:05:46 -0800

    Update generate_scoped_keys.py

--- generate_scoped_keys.py
+++ generate_scoped_keys.py
@@ -1,10 +1,18 @@
 from keen.client import KeenClient
 from keen import scoped_keys
     
-api_key='master api key'
+api_key='7EC010A4C033AF0EAC60B13AEA0EF1D7'
     
-write_key = scoped_keys.encrypt(api_key, {"allowed_operations": ["write"]})
-read_key = scoped_keys.encrypt(api_key, {"allowed_operations": ["read"]})
+write_key = scoped_keys.encrypt(api_key, {"filters": [{
+                                                      "property_name": "unit_domain",
+                                                      "operator": "eq",
+                                                      "property_value": "fem-inc.com"
+                                                      }],"allowed_operations": ["write"]})
+read_key = scoped_keys.encrypt(api_key, {"filters": [{
+                                                     "property_name": "unit_domain",
+                                                     "operator": "eq",
+                                                     "property_value": "fem-inc.com"
+                                                     }],"allowed_operations": ["read"]})
 
 
 print(write_key)
